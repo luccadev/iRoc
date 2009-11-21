fa49c0338fdc221f00fb30b575378a9749786d53
Jean-Michel Fischer <jmf@macbook.local>
2009-11-21 16:03:30 +0100

    Slider Improvement

--- Classes/iRocTouchView.m
+++ Classes/iRocTouchView.m
@@ -67,34 +67,36 @@ void CGContextAddRoundedRect (CGContextRef c, CGRect rect, int corner_radius) {
 	if(x >258)
 		x = 258;
 	
+	int off = 20;
+	
 	// Bar
 	CGContextSetRGBFillColor(context, .1, .1, .1, 1); 
-	rectVel = CGRectMake(9, 54, 262, 12);
+	rectVel = CGRectMake(9+off, 54, 262, 12);
 	CGContextFillRect(context, rectVel);
 	CGContextSetRGBFillColor(context, .2, .2, .2, 1); 
-	rectVel = CGRectMake(10, 55, 260, 10);
+	rectVel = CGRectMake(10+off, 55, 260, 10);
 	CGContextFillRect(context, rectVel);
 	
 	//Knob
 	CGContextSetRGBFillColor(context, .3, .3, .3, 1);  
-	CGContextAddRoundedRect(context, CGRectMake(x-20,10,40,100), 5);  
+	CGContextAddRoundedRect(context, CGRectMake(x-20+off,10,40,100), 5);  
 	CGContextFillPath(context);  
 	
 	//Knob border
 	CGContextSetLineWidth(context, 0.5);  
 	CGContextSetRGBStrokeColor(context, .5, .5, .5, 1);  
-	CGContextAddRoundedRect(context, CGRectMake(x-20,10,40,100), 5);  
+	CGContextAddRoundedRect(context, CGRectMake(x-20+off,10,40,100), 5);  
 	CGContextStrokePath(context);  
 	
 	//Knob lines
 	CGContextSetRGBFillColor(context, .2, .2, .2, 1); 
-	CGContextFillRect(context, CGRectMake(x-11,15,1,90));
-	CGContextFillRect(context, CGRectMake(x-1,15,1,90));
-	CGContextFillRect(context, CGRectMake(x+9,15,1,90));
+	CGContextFillRect(context, CGRectMake(x-11+off,15,1,90));
+	CGContextFillRect(context, CGRectMake(x-1+off,15,1,90));
+	CGContextFillRect(context, CGRectMake(x+9+off,15,1,90));
 	CGContextSetRGBFillColor(context, .4, .4, .4, 1); 
-	CGContextFillRect(context, CGRectMake(x-10,15,1,90));
-	CGContextFillRect(context, CGRectMake(x,15,1,90));
-	CGContextFillRect(context, CGRectMake(x+10,15,1,90));
+	CGContextFillRect(context, CGRectMake(x-10+off,15,1,90));
+	CGContextFillRect(context, CGRectMake(x+off,15,1,90));
+	CGContextFillRect(context, CGRectMake(x+10+off,15,1,90));
 
 	
 	}
@@ -104,6 +106,8 @@ void CGContextAddRoundedRect (CGContextRef c, CGRect rect, int corner_radius) {
 	// We only support single touches, so anyObject retrieves just that touch from touches
 	UITouch *touch = [touches anyObject];
 
+	
+	
 	x = [touch locationInView:self].x;
 	y = [touch locationInView:self].y;
 	
