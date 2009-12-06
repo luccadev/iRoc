5fbaba5e1c074ee17d81c395187e75a63d0616fb
Jean-Michel Fischer <jmf@macbook.local>
2009-12-06 15:53:31 +0100

    added about view

---
+++ Classes/iRocAboutView.h
@@ -0,0 +1,18 @@
+//
+//  iRocAboutView.h
+//  iRoc
+//
+//  Created by Jean-Michel Fischer on 06.12.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface iRocAboutView : UIViewController {
+	IBOutlet UILabel *labelVersion;
+}
+
+@property (nonatomic, retain) UILabel *labelVersion;
+
+@end
